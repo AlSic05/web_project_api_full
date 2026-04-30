c6d3d6d36788278dba9d17d6c4a6954c0b19f735
AlejandraSichaca <aleja.sichaca@hotmail.com>
2026-04-30 11:45:03 -0500

    11. Configurar la autorización en el front-end

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,7 +1,13 @@
 class Api {
   constructor(options) {
     this._baseUrl = options.baseUrl;
-    this._headers = options.headers;
+  }
+
+  _getHeaders() {
+    return {
+      authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      "Content-Type": "application/json",
+    };
   }
 
   _checkResponse(res) {
@@ -13,88 +19,64 @@ class Api {
 
   getUserInfo() {
     return fetch(this._baseUrl + "/users/me", {
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._checkResponse);
   }
 
   getInitialCards() {
     return fetch(this._baseUrl + "/cards", {
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._checkResponse);
   }
 
   setUserInfo(name, about) {
     return fetch(this._baseUrl + "/users/me", {
       method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        name: name,
-        about: about,
-      }),
+      headers: this._getHeaders(),
+      body: JSON.stringify({ name, about }),
     }).then(this._checkResponse);
   }
 
   addCard(name, link) {
     return fetch(this._baseUrl + "/cards", {
       method: "POST",
-      headers: this._headers,
-      body: JSON.stringify({
-        name: name,
-        link: link,
-      }),
+      headers: this._getHeaders(),
+      body: JSON.stringify({ name, link }),
     }).then(this._checkResponse);
   }
 
   addLike(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: "PUT",
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._checkResponse);
   }
 
   removeLike(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._checkResponse);
-  }
-
-  changeLikeCardStatus(cardId, isLiked) {
-    if (isLiked) {
-      return this.addLike(cardId);
-    } else {
-      return this.removeLike(cardId);
-    }
   }
 
   deleteCard(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._checkResponse);
   }
 
   updateAvatar(avatarLink) {
     return fetch(this._baseUrl + "/users/me/avatar", {
       method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        avatar: avatarLink,
-      }),
+      headers: this._getHeaders(),
+      body: JSON.stringify({ avatar: avatarLink }),
     }).then(this._checkResponse);
   }
 }
 
-export function getAppInfo(api) {
-  return Promise.all([api.getUserInfo(), api.getInitialCards()]);
-}
-
 const api = new Api({
   baseUrl: "https://around-api.es.tripleten-services.com/v1",
-  headers: {
-    authorization: "f259293f-0caf-46de-b937-becf0e8d1736",
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
