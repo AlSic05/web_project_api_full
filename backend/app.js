75d897fd9c3d4a39b50048f984346e8bd2cfd125
AlejandraSichaca <aleja.sichaca@hotmail.com>
2026-04-30 09:40:49 -0500

    4. Crear una ruta para el inicio de sesión y el registro

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-
-mongoose.connect("mongodb://localhost:27017/aroundb");
+import { login, createUser } from "./controllers/users.js";
 
 import userrouter from "./routes/users.js";
 import cardrouter from "./routes/cards.js";
+
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use(userrouter);
+app.use((req, res, next) => {
+  req.user = {
+    _id: "69d3d3bcaf75603d3792d4e1",
+  };
+  next();
+});
 
+app.post("/signin", login);
+app.post("/signup", createUser);
+
+app.use(userrouter);
 app.use(cardrouter);
 
 app.use((req, res) => {
@@ -23,12 +33,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
-});
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: "69d3d3bcaf75603d3792d4e1",
-  };
-
-  next();
 });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { getUsers, getUserById, createUser } from "../controllers/users.js";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  login,
+} from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 
 router.get("/users/:userId", getUserById);
-
-router.post("/users", createUser);
 
 export default router;
